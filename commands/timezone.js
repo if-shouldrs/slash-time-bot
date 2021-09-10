679f2179e29e604f240902ae82563ac1f7bd8bb1
if-shouldrs <71544385+if-shouldrs@users.noreply.github.com>
2021-09-10 12:10:06 +0100

    Improve `timezone` command
    Made `tz` an optional argument, either telling the user his current timezone or how to get a valid one, when the argument is omitted

--- commands/timezone.js
+++ commands/timezone.js
@@ -18,17 +18,32 @@ module.exports = {
         .addStringOption((option) =>
             option
                 .setName("tz")
-                .setDescription("The TZ Database name for your timezone")
-                .setRequired(true),
+                .setDescription("The TZ Database name for your timezone"),
         ),
     async execute(interaction) {
         const tz = interaction.options.getString("tz");
+        if (tz === null) {
+            // eslint-disable-next-line func-names
+            db.get(interaction.user.id, function (err, tz) {
+                let response;
+                if (err) {
+                    // No timezone given and no timezone in db, help user find one
+                    response =
+                        "Please set your timezone to a valid tz code (see here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)";
+                } else {
+                    // No timezone given, inform user of current timezone
+                    response = `Your current timezone is \`${tz}\`.`;
+                }
+                interaction.reply({ content: response, ephemeral: true });
+            });
+            return;
+        }
 
-        console.log(`adjusting user's timezone to ${tz}`);
+        // Update user's timezone in db
         await db.put(interaction.user.id, tz);
 
-        const stamp = `<t:${dayjs().tz(tz).unix()}:f>`;
-        const response = `Timezone successfully set to \`${tz}\`\n${stamp}\n\`${stamp}\``;
+        // Inform user of successful change
+        const response = `Timezone successfully set to \`${tz}\`.`;
         await interaction.reply({ content: response, ephemeral: true });
     },
 };
